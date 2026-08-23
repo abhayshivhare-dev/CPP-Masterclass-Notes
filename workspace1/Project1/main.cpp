ba17493ee90cea50c800c7142ffb3e97116f0ce0
Abhay Shivhare <abhay.shivhare.official@gmail.com>
2026-08-23 12:32:23 +0530

    Add gitignore to remove cache and build files

--- workspace1/Project1/main.cpp
+++ workspace1/Project1/main.cpp
@@ -4,38 +4,38 @@ using namespace std;
 
 int main() {
     
-   // cout << "Hello world! ";
+    cout << "Hello world! ";
    
-//   cout << "Hello";
-   //cout << "world" << endl;
+    cout << "Hello";
+    cout << "world" << endl;
    
-   //cout << "Hello World!" << endl;
-   //cout << "Hello!" << "world" << endl;
-   //cout << "Hello world!\n";
-   //cout << "hello\nout\nthere\n";
+    cout << "Hello World!" << endl;
+    cout << "Hello!" << "world" << endl;
+    cout << "Hello world!\n";
+    cout << "hello\nout\nthere\n";
    
-  //  int num1;
+    int num1;
     int num2;
     double num3;
      
-//     cout << "enter your first enterger: ";
-//     cin >> num1;
-//     cout << "your integer is: " << num1 << endl;
+    cout << "enter your first integer: ";
+    cin >> num1;
+    cout << "your integer is: " << num1 << endl;
 
-//    cout << "your first integer is: ";
-//    cin >> num1;
-//    cout << "Your second integer is: ";
-//    cin >> num2;
-//    
-//    cout << "You entered " << num1 << " and " << num2 << endl;
-
-//    cout << "Enter two integers with a space: ";
-//    cin >> num1 >> num2;
-//    cout << "You Entered: " << num1 << " and " << num2 << endl;
+    cout << "your first integer is: ";
+    cin >> num1;
+    cout << "Your second integer is: ";
+    cin >> num2;
     
-//    cout << "Enter a double: ";
-//    cin >> num3;
-//    cout << "You entered: " << num3 << endl;
+    cout << "You entered " << num1 << " and " << num2 << endl;
+
+    cout << "Enter two integers with a space: ";
+    cin >> num1 >> num2;
+    cout << "You Entered: " << num1 << " and " << num2 << endl;
+    
+    cout << "Enter a double: ";
+    cin >> num3;
+    cout << "You entered: " << num3 << endl;
     
     cout << "Enter an integer: ";
     cin >> num2;
